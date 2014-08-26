e06ca03a45b0fdf85a8b0cbd665a0d7669ee7042
Fredrik Steen <fredrik.steen@redpill-linpro.com>
2014-08-26 15:16:35 +0200

    Bugfix: MASH_STEPS not getting parsed

--- beerxml_test.go
+++ beerxml_test.go
@@ -31,14 +31,18 @@ func TestRecipesRecipe(t *testing.T) {
 	}
 
 	if bxml.Recipes[0].Name != "Burton Ale" {
-		t.Error("Recipe.Name failed")
+		t.Error("Recipe.Name")
 	}
 
 	if bxml.Recipes[0].Hops[0].Hsi != "35.0" {
-		t.Error("Recipe.Hops.Hsi failed")
+		t.Error("Recipe.Hops.Hsi")
 	}
 
 	if bxml.Recipes[0].Fermentables[0].Amount != 3.628736 {
-		t.Error("Recipe.Fermentables.Amount faild")
+		t.Error("Recipe.Fermentables.Amount")
+	}
+
+	if bxml.Recipes[0].Mash.MashSteps[0].StepTime != 45 {
+		t.Error("Recipes.Mash.MashSteps.StepTime")
 	}
 }
